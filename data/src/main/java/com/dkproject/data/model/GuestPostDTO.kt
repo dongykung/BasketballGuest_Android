675220fd0013aa1f329031f25d952b4669ec360a
kim dong kyung <kdkd422@naver.com>
2025-02-04 22:18:49 +0900

    [Add] 게스트 글 데이터 모델

---
+++ data/src/main/java/com/dkproject/data/model/GuestPostDTO.kt
@@ -0,0 +1,41 @@
+package com.dkproject.data.model
+
+import com.dkproject.domain.model.GuestPost
+import com.google.firebase.firestore.DocumentId
+import java.util.Date
+
+data class GuestPostDTO(
+    @DocumentId val id: String?,
+    val date: Date,
+    val description: String,
+    val endDate: Date,
+    val lat: Double,
+    val lng: Double,
+    val memberCount: Int,
+    val parkFlag: String,
+    val placeAddress: String,
+    val placeName: String,
+    val positions: List<String>,
+    val startDate: Date,
+    val title: String,
+    val writerUid: String
+)
+
+fun GuestPostDTO.toDomain(): GuestPost {
+    return GuestPost(
+        id = id,
+        date = date,
+        description = description,
+        endDate = endDate,
+        lat = lat,
+        lng = lng,
+        memberCount = memberCount,
+        parkFlag = parkFlag,
+        placeAddress = placeAddress,
+        placeName = placeName,
+        positions = positions,
+        startDate = startDate,
+        title = title,
+        writerUid = writerUid
+    )
+}
