9529b7caccf1f954a47c0cddb495062b0ca8bacd
kim dong kyung <kdkd422@naver.com>
2025-02-04 10:41:51 +0900

    [Add] AuthError 타입

---
+++ presentation/src/main/java/com/dkproject/presentation/ui/screen/login/AuthError.kt
@@ -0,0 +1,6 @@
+package com.dkproject.presentation.ui.screen.login
+
+import androidx.compose.ui.res.stringResource
+
+class NoCredentialsException(message: String) : Exception(message)
+class LoginFailed(message: String) : Exception(message)
