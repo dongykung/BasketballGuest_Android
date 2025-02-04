aa5eeaac5910581c1b4dc2a7deffed915c44b541
kim dong kyung <kdkd422@naver.com>
2025-02-04 10:42:05 +0900

    [Add] AppNavGraph 앱 네비게이션

---
+++ presentation/src/main/java/com/dkproject/presentation/navigation/AppNavGraph.kt
@@ -0,0 +1,24 @@
+package com.dkproject.presentation.navigation
+
+import android.content.Context
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun AppNavGraph(navController: NavHostController,
+                modifier: Modifier = Modifier,
+                snackbarHostState: SnackbarHostState) {
+    NavHost(navController = navController, startDestination = Screen.SignUp) {
+        authNavGraph(navController = navController, snackbarHostState = snackbarHostState)
+        HomeNavGraph(navController = navController, modifier = modifier)
+    }
+}
+
