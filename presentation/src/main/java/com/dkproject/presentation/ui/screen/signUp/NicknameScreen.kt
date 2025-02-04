da09c0d858046b4ee02e06f64b8bebf777a99b2a
kim dong kyung <kdkd422@naver.com>
2025-02-04 10:47:01 +0900

    [Add] 회원가입 - 닉네임

---
+++ presentation/src/main/java/com/dkproject/presentation/ui/screen/signUp/NicknameScreen.kt
@@ -0,0 +1,72 @@
+package com.dkproject.presentation.ui.screen.signUp
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.dkproject.presentation.R
+import com.dkproject.presentation.ui.component.button.DefaultButton
+import com.dkproject.presentation.ui.component.DefaultTextField
+import com.dkproject.presentation.ui.theme.AppTheme
+
+@Composable
+fun NicknameScreen(
+    nickname: String,
+    loading: Boolean,
+    onNicknameChange: (String) -> Unit,
+    onConfirmClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.Start
+    ) {
+
+        Text(text = stringResource(R.string.inputnickname),
+            style = MaterialTheme.typography.headlineMedium)
+        Spacer(modifier = Modifier.height(24.dp))
+        DefaultTextField(
+            value = nickname,
+            onValueChange = onNicknameChange,
+            label = stringResource(R.string.nickname),
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(48.dp))
+        DefaultButton(
+            title = stringResource(R.string.next),
+            loading = loading,
+            onClick = onConfirmClick,
+            enabled = nickname.isNotEmpty(),
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun NicknameScreenPreview() {
+    AppTheme {
+        NicknameScreen(
+            nickname = "df",
+            loading = false,
+            onNicknameChange = {},
+            onConfirmClick = {},
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        )
+    }
+}
