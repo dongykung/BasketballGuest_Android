394876af9eb248757356e32dee276b8e4b17fee0
kim dong kyung <kdkd422@naver.com>
2025-02-04 22:15:55 +0900

    [Feat] 유저데이터 업로드 함수 추가

--- domain/src/main/java/com/dkproject/domain/repository/AuthRepository.kt
+++ domain/src/main/java/com/dkproject/domain/repository/AuthRepository.kt
@@ -1,7 +1,10 @@
 package com.dkproject.domain.repository
 
+import com.dkproject.domain.model.User
+
 interface AuthRepository {
     suspend fun checkUserUid(uid: String): Boolean
     /// true 반환 시 닉네임 사용 가능
     suspend fun checkUserNickName(nickName: String): Boolean
+    suspend fun uploadUserData(user: User): Result<Unit>
 }
