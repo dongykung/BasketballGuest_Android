f914d0e7de0029d11d21536c367b8fd468c6c629
kim dong kyung <kdkd422@naver.com>
2025-02-04 22:19:32 +0900

    [Style] MapaceScreen -> MyPageScreen 오타 수정

--- presentation/src/main/java/com/dkproject/presentation/navigation/HomeNavGraph.kt
+++ presentation/src/main/java/com/dkproject/presentation/navigation/HomeNavGraph.kt
@@ -3,23 +3,17 @@ package com.dkproject.presentation.navigation
 import android.annotation.SuppressLint
 import android.util.Log
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.toRoute
-import com.dkproject.presentation.ui.screen.myPage.MyPaceScreen
-import com.dkproject.presentation.ui.screen.splash.SplashScreen
+import com.dkproject.presentation.ui.screen.myPage.MyPageScreen
 
 @SuppressLint("ComposableDestinationInComposeScope")
 fun NavGraphBuilder.HomeNavGraph(navController: NavController,modifier: Modifier = Modifier) {
@@ -43,7 +37,7 @@ fun NavGraphBuilder.HomeNavGraph(navController: NavController,modifier: Modifier
         }
     }
     composable(BottomNavItem.Profile.route) {
-        MyPaceScreen(moveToLogin = {
+        MyPageScreen(moveToLogin = {
             navController.navigate(Screen.Login) {
                 popUpTo(navController.graph.id) {
                     inclusive = true
