edbc35b173144f0b6a14b0278f0e9b88e05f7383
kim dong kyung <kdkd422@naver.com>
2025-02-04 22:17:59 +0900

    [Feat] label -> placeholder로 변경, 포커스 컨테이너 색상 변경

--- presentation/src/main/java/com/dkproject/presentation/ui/component/DefaultTextField.kt
+++ presentation/src/main/java/com/dkproject/presentation/ui/component/DefaultTextField.kt
@@ -1,15 +1,19 @@
 package com.dkproject.presentation.ui.component
 
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun DefaultTextField(
@@ -18,18 +22,21 @@ fun DefaultTextField(
     label: String,
     maxLines: Int = 1,
     keyboardOptions: KeyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Done),
+    shape: Shape = RoundedCornerShape(16.dp),
     modifier: Modifier = Modifier
 ) {
     TextField(
         value = value,
         onValueChange = onValueChange,
-        label = { Text(label) },
+        placeholder = { Text(label) },
         maxLines = maxLines,
         keyboardOptions = keyboardOptions,
         colors = TextFieldDefaults.colors(
             focusedIndicatorColor = Color.Transparent,
-            unfocusedIndicatorColor = Color.Transparent
+            unfocusedIndicatorColor = Color.Transparent,
+            focusedContainerColor = MaterialTheme.colorScheme.secondaryContainer
         ),
-        modifier = modifier
+        modifier = modifier,
+        shape = shape,
     )
 }
