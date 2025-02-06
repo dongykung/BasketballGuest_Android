0985d195c1be9aecc90790b4ff002c04ba7e3faf
kim dong kyung <kdkd422@naver.com>
2025-02-07 03:22:37 +0900

    [Feat] 게스트 모듈

---
+++ data/src/main/java/com/dkproject/data/di/GuestModule.kt
@@ -0,0 +1,28 @@
+package com.dkproject.data.di
+
+import com.dkproject.data.Repository.GuestRepositoryImpl
+import com.dkproject.data.Repository.SearchPlaceRepositoryImpl
+import com.dkproject.domain.repository.GuestRepository
+import com.dkproject.domain.repository.SearchPlaceRepository
+import com.dkproject.domain.usecase.Guest.UploadGuestPostUseCase
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+
+@Module
+@InstallIn(ViewModelComponent::class)
+object GuestUseCaseModule {
+    @Provides
+    fun provideUploadGuestPostUseCase(guestRepository: GuestRepository): UploadGuestPostUseCase {
+        return UploadGuestPostUseCase(guestRepository)
+    }
+}
+
+@Module
+@InstallIn(ViewModelComponent::class)
+abstract class GuestRepositoryModule {
+    @Binds
+    abstract fun bindGuestRepository(guestRepositoryImpl: GuestRepositoryImpl): GuestRepository
+}
