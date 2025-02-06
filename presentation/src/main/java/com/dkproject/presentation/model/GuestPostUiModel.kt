700ead8ad676d1686a16ab5b5f2a15eebef0c849
kim dong kyung <kdkd422@naver.com>
2025-02-07 03:23:58 +0900

    [Chore] GuestUiModel

--- presentation/src/main/java/com/dkproject/presentation/model/GuestPostUiModel.kt
+++ presentation/src/main/java/com/dkproject/presentation/model/GuestPostUiModel.kt
@@ -1,5 +1,6 @@
 package com.dkproject.presentation.model
 
+import com.dkproject.domain.model.GuestPost
 import java.util.Date
 
 data class GuestPostUiModel(
@@ -10,7 +11,7 @@ data class GuestPostUiModel(
     val lat: Double = 0.0,
     val lng: Double = 0.0,
     val memberCount: Int = 0,
-    val parkFlag: String = "",
+    val parkFlag: Int = 0,
     val placeAddress: String = "",
     val placeName: String = "",
     val positions: List<String> = emptyList(),
@@ -18,3 +19,22 @@ data class GuestPostUiModel(
     val title: String = "",
     val writerUid: String = ""
 )
+
+fun GuestPostUiModel.toDomainModel(): GuestPost {
+    return GuestPost(
+        id = id,
+        date = date,
+        description = description,
+        endDate = endDate,
+        lat = lat,
+        lng = lng,
+        memberCount = memberCount,
+        parkFlag = parkFlag,
+        placeAddress = placeAddress,
+        placeName = placeName,
+        positions = positions,
+        startDate = startDate,
+        title = title,
+        writerUid = writerUid
+    )
+}
