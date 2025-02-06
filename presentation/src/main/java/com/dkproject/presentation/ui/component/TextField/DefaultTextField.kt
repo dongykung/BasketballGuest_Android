413060ee56a97576670e1c7b7c704f82e288efd7
kim dong kyung <kdkd422@naver.com>
2025-02-07 03:21:10 +0900

    [Chore] 글꼴 스타일 추가

--- presentation/src/main/java/com/dkproject/presentation/ui/component/TextField/DefaultTextField.kt
+++ presentation/src/main/java/com/dkproject/presentation/ui/component/TextField/DefaultTextField.kt
@@ -1,9 +1,8 @@
-package com.dkproject.presentation.ui.component
+package com.dkproject.presentation.ui.component.TextField
 
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -12,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 
@@ -23,6 +23,7 @@ fun DefaultTextField(
     maxLines: Int = 1,
     keyboardOptions: KeyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Done),
     shape: Shape = RoundedCornerShape(16.dp),
+    textStyle: TextStyle = LocalTextStyle.current,
     modifier: Modifier = Modifier
 ) {
     TextField(
@@ -34,8 +35,9 @@ fun DefaultTextField(
         colors = TextFieldDefaults.colors(
             focusedIndicatorColor = Color.Transparent,
             unfocusedIndicatorColor = Color.Transparent,
-            focusedContainerColor = MaterialTheme.colorScheme.secondaryContainer
+            //focusedContainerColor = MaterialTheme.colorScheme.secondaryContainer
         ),
+        textStyle = textStyle,
         modifier = modifier,
         shape = shape,
     )
