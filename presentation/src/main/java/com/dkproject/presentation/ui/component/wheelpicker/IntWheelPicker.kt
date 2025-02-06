9b4db2a7ef768ba8813a7b8282143d9d15bc7a96
kim dong kyung <kdkd422@naver.com>
2025-02-07 03:24:41 +0900

    [Chore] 안쓰는 import 삭제

--- presentation/src/main/java/com/dkproject/presentation/ui/component/wheelpicker/IntWheelPicker.kt
+++ presentation/src/main/java/com/dkproject/presentation/ui/component/wheelpicker/IntWheelPicker.kt
@@ -1,6 +1,5 @@
-package com.dkproject.presentation.ui.component
+package com.dkproject.presentation.ui.component.wheelpicker
 
-import android.util.Log
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.background
@@ -11,9 +10,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.Text
@@ -26,9 +23,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.draw.scale
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
