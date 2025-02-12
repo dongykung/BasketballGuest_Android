e2abf55b26a11e09f0304c321264c69ff11c3af9
kim dong kyung <kdkd422@naver.com>
2025-02-12 15:14:13 +0900

    [Feat] 홈 탑바

---
+++ presentation/src/main/java/com/dkproject/presentation/ui/component/HomeTopAppBar.kt
@@ -0,0 +1,36 @@
+package com.dkproject.presentation.ui.component
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.res.stringResource
+import androidx.navigation.NavController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import com.dkproject.presentation.R
+import com.dkproject.presentation.navigation.BottomNavItem
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeTopAppBar(
+    navController: NavController,
+    scrollBehavior: TopAppBarScrollBehavior
+) {
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
+    AnimatedVisibility(currentRoute == BottomNavItem.Guest.route) {
+        TopAppBar(
+            title = {
+                Text(
+                    text = stringResource(R.string.lookingforguest),
+                    style = MaterialTheme.typography.titleLarge
+                )
+            },
+            scrollBehavior = scrollBehavior
+        )
+    }
+}
