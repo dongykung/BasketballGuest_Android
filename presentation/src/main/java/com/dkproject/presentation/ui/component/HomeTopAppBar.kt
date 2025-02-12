6abbec90b678d3b3d6bb5e2cad36621521ba89ef
kim dong kyung <kdkd422@naver.com>
2025-02-12 18:40:20 +0900

    [Chore] 컨테이너 색상 백그라운드

--- presentation/src/main/java/com/dkproject/presentation/ui/component/HomeTopAppBar.kt
+++ presentation/src/main/java/com/dkproject/presentation/ui/component/HomeTopAppBar.kt
@@ -5,6 +5,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -30,7 +31,8 @@ fun HomeTopAppBar(
                     style = MaterialTheme.typography.titleLarge
                 )
             },
-            scrollBehavior = scrollBehavior
+            scrollBehavior = scrollBehavior,
+            colors = TopAppBarDefaults.topAppBarColors(containerColor = MaterialTheme.colorScheme.background)
         )
     }
 }
