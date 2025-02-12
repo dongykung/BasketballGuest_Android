042e649fe85080031814c3793a9b1a791bfeabf6
kim dong kyung <kdkd422@naver.com>
2025-02-12 15:13:28 +0900

    [Feat] 게스트 구인 가져오기 함수 추가 - 페이징 소스 연결

--- data/src/main/java/com/dkproject/data/Repository/GuestRepositoryImpl.kt
+++ data/src/main/java/com/dkproject/data/Repository/GuestRepositoryImpl.kt
@@ -1,15 +1,31 @@
 package com.dkproject.data.Repository
 
+import android.content.Context
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import com.dkproject.data.data.paging.GuestPagingSource
 import com.dkproject.data.model.toDTO
+import com.dkproject.domain.model.GuestFilter
 import com.dkproject.domain.model.GuestPost
 import com.dkproject.domain.repository.GuestRepository
 import com.google.firebase.firestore.FirebaseFirestore
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class GuestRepositoryImpl @Inject constructor(
-    private val firestore: FirebaseFirestore
+    private val firestore: FirebaseFirestore,
+    @ApplicationContext private val context: Context
 ): GuestRepository {
     override suspend fun uploadGuestPost(guestPost: GuestPost):Result<Unit> = kotlin.runCatching {
         firestore.collection("Guest").add(guestPost.toDTO())
+    }
+
+    override fun getGuestPostList(guestFilter: GuestFilter): Flow<PagingData<GuestPost>> {
+        return Pager(
+            config = PagingConfig(pageSize = 10, initialLoadSize = 10, prefetchDistance = 2),
+            pagingSourceFactory = {GuestPagingSource(guestFilter = guestFilter, firestore = firestore, context = context) }
+        ).flow
     }
 }
