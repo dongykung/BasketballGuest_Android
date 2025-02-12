c354039b1b5e50d291eaec0e4e9da69b312b6fe5
kim dong kyung <kdkd422@naver.com>
2025-02-12 15:13:21 +0900

    [Feat] 게스트 구인 가져오기 함수 추가

--- domain/src/main/java/com/dkproject/domain/repository/GuestRepository.kt
+++ domain/src/main/java/com/dkproject/domain/repository/GuestRepository.kt
@@ -1,7 +1,11 @@
 package com.dkproject.domain.repository
 
+import androidx.paging.PagingData
+import com.dkproject.domain.model.GuestFilter
 import com.dkproject.domain.model.GuestPost
+import kotlinx.coroutines.flow.Flow
 
 interface GuestRepository {
     suspend fun uploadGuestPost(guestPost: GuestPost): Result<Unit>
+    fun getGuestPostList(guestFilter: GuestFilter): Flow<PagingData<GuestPost>>
 }
