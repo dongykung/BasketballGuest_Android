5636952e4e1279dbe6ee2a22b79c430f04d4ac5b
kim dong kyung <kdkd422@naver.com>
2025-02-12 18:38:34 +0900

    [Feat] 인원 모집 슬라이더 추가

--- presentation/src/main/java/com/dkproject/presentation/ui/screen/GuestPost/GuestPositionScreen.kt
+++ presentation/src/main/java/com/dkproject/presentation/ui/screen/GuestPost/GuestPositionScreen.kt
@@ -1,20 +1,23 @@
 package com.dkproject.presentation.ui.screen.GuestPost
 
-import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Slider
+import androidx.compose.material3.SliderDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -28,11 +31,15 @@ import com.dkproject.presentation.R
 import com.dkproject.presentation.model.Position
 import com.dkproject.presentation.ui.component.button.DefaultButton
 import com.dkproject.presentation.ui.theme.AppTheme
+import kotlin.math.max
+import kotlin.math.roundToInt
 
 @Composable
 fun GuestPositionScreen(
     modifier: Modifier = Modifier,
     positions: List<String>,
+    memberCount: Int,
+    memberCountChange: (Int) -> Unit = {},
     onPositionSelected: (Position) -> Unit,
     onConfirmClick: () -> Unit = {}
 ) {
@@ -53,7 +60,7 @@ fun GuestPositionScreen(
         ) {
             items(Position.entries.toTypedArray()) { position ->
                 val animatedColor by animateColorAsState(
-                    targetValue = if (positions.contains(position.toFirestoreValue())) Color(0xFFFF8C00 ) else Color.Unspecified,
+                    targetValue = if (positions.contains(position.toFirestoreValue())) Color(0xFFFF8C00 ) else MaterialTheme.colorScheme.inverseSurface,
                     label = "",
                 )
                 Button(
@@ -66,6 +73,33 @@ fun GuestPositionScreen(
                     )
                 }
             }
+        }
+        Spacer(modifier = Modifier.height(32.dp))
+        Text(
+            text = stringResource(R.string.memberCount),
+            style = MaterialTheme.typography.titleSmall
+        )
+        Spacer(modifier = Modifier.height(32.dp))
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Slider(
+                value = memberCount.toFloat(),
+                onValueChange = {
+                    val discreteValue = max(it.roundToInt(), 1)
+                    memberCountChange(discreteValue)
+                },
+                colors = SliderDefaults.colors(
+                    thumbColor = MaterialTheme.colorScheme.inverseSurface,
+                    inactiveTickColor = MaterialTheme.colorScheme.tertiaryContainer,
+                    inactiveTrackColor = MaterialTheme.colorScheme.inverseSurface,
+                    activeTickColor = MaterialTheme.colorScheme.inverseSurface,
+                    activeTrackColor = MaterialTheme.colorScheme.secondaryContainer,
+                ),
+                valueRange = 0f..15f,
+                steps = 14,
+                modifier = Modifier.weight(1f)
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(text = memberCount.toString(), style = MaterialTheme.typography.bodyLarge)
         }
         Spacer(modifier = Modifier.height(32.dp))
 
@@ -86,7 +120,8 @@ private fun GuestPositionScreenPreview() {
             .fillMaxSize()
             .padding(16.dp),
             positions = emptyList(),
-            onPositionSelected = {}
+            onPositionSelected = {},
+            memberCount = 2
         )
     }
 }
