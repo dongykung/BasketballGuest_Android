4840483dac23c82cf5c5fd966d669739f28e017d
kim dong kyung <kdkd422@naver.com>
2025-02-12 15:14:22 +0900

    [Feat] 탑바 스크롤 연결

--- presentation/src/main/java/com/dkproject/presentation/activity/MainActivity.kt
+++ presentation/src/main/java/com/dkproject/presentation/activity/MainActivity.kt
@@ -3,37 +3,44 @@ package com.dkproject.presentation.activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
-import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.navigation.compose.rememberNavController
 import com.dkproject.presentation.navigation.AppNavGraph
 import com.dkproject.presentation.ui.component.BottomNavigation
-import com.dkproject.presentation.ui.component.MyFloatingButton
+import com.dkproject.presentation.ui.component.HomeTopAppBar
+import com.dkproject.presentation.ui.component.button.MyFloatingButton
+import com.dkproject.presentation.ui.screen.searchAddress.SearchAddressScreen
 import com.dkproject.presentation.ui.theme.AppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity: ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             AppTheme {
                 val navController = rememberNavController()
                 val snackbarHostState = remember { SnackbarHostState() }
+                val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
                 Scaffold(
+                    modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+                    topBar = { HomeTopAppBar(navController = navController, scrollBehavior = scrollBehavior) },
                     bottomBar = { BottomNavigation(navController = navController) },
                     snackbarHost = { SnackbarHost(snackbarHostState) },
                     floatingActionButton = { MyFloatingButton(navController = navController) }
                 ) { innerPadding ->
                     AppNavGraph(navController = navController,
-                        modifier = Modifier.padding(innerPadding),
+                        modifier = Modifier.fillMaxSize().padding(innerPadding),
                         snackbarHostState = snackbarHostState
                     )
                 }
