8ea6b043a7116a22ab68c88449b938462f52c2f2
kim dong kyung <kdkd422@naver.com>
2025-02-12 18:39:42 +0900

    [Feat] Date객체 직렬 위해 커스텀 Serializer 연결

--- presentation/src/main/java/com/dkproject/presentation/model/GuestPostUiModel.kt
+++ presentation/src/main/java/com/dkproject/presentation/model/GuestPostUiModel.kt
@@ -1,20 +1,28 @@
 package com.dkproject.presentation.model
 
 import com.dkproject.domain.model.GuestPost
+import com.dkproject.presentation.util.DateSerializer
+import kotlinx.serialization.Serializable
+import java.time.Instant
 import java.util.Date
 
+@Serializable
 data class GuestPostUiModel(
     val id: String? = null,
+    @Serializable(with = DateSerializer::class)
     val date: Date = Date(),
     val description: String = "",
+    @Serializable(with = DateSerializer::class)
     val endDate: Date = Date(),
     val lat: Double = 0.0,
     val lng: Double = 0.0,
-    val memberCount: Int = 0,
+    val currentMember: Int = 0,
+    val memberCount: Int = 1,
     val parkFlag: Int = 0,
     val placeAddress: String = "",
     val placeName: String = "",
     val positions: List<String> = emptyList(),
+    @Serializable(with = DateSerializer::class)
     val startDate: Date = Date(),
     val title: String = "",
     val writerUid: String = ""
@@ -28,6 +36,7 @@ fun GuestPostUiModel.toDomainModel(): GuestPost {
         endDate = endDate,
         lat = lat,
         lng = lng,
+        currentMember = currentMember,
         memberCount = memberCount,
         parkFlag = parkFlag,
         placeAddress = placeAddress,
@@ -47,6 +56,7 @@ fun GuestPost.toUiModel(): GuestPostUiModel {
         endDate = endDate,
         lat = lat,
         lng = lng,
+        currentMember = currentMember,
         memberCount = memberCount,
         parkFlag = parkFlag,
         placeAddress = placeAddress,
