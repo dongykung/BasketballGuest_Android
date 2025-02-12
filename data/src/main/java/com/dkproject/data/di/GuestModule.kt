d3534cbf01f67ebf6d174fb56b8a26308e5b4afa
kim dong kyung <kdkd422@naver.com>
2025-02-12 15:11:30 +0900

    [DI] 게스트 모듈

--- data/src/main/java/com/dkproject/data/di/GuestModule.kt
+++ data/src/main/java/com/dkproject/data/di/GuestModule.kt
@@ -4,6 +4,7 @@ import com.dkproject.data.Repository.GuestRepositoryImpl
 import com.dkproject.data.Repository.SearchPlaceRepositoryImpl
 import com.dkproject.domain.repository.GuestRepository
 import com.dkproject.domain.repository.SearchPlaceRepository
+import com.dkproject.domain.usecase.Guest.GetGuestPostListUseCase
 import com.dkproject.domain.usecase.Guest.UploadGuestPostUseCase
 import dagger.Binds
 import dagger.Module
@@ -17,6 +18,11 @@ object GuestUseCaseModule {
     @Provides
     fun provideUploadGuestPostUseCase(guestRepository: GuestRepository): UploadGuestPostUseCase {
         return UploadGuestPostUseCase(guestRepository)
+    }
+
+    @Provides
+    fun provideGetGuestPostListUseCase(guestRepository: GuestRepository): GetGuestPostListUseCase {
+        return GetGuestPostListUseCase(guestRepository)
     }
 }
 
