de02efca7b5d949adbfe520f54f5d2d924ca7e37
kim dong kyung <kdkd422@naver.com>
2025-02-13 18:52:08 +0900

    [Feat] 필터링 기능 적용

--- presentation/src/main/java/com/dkproject/presentation/ui/component/GuestFilterTopBar.kt
+++ presentation/src/main/java/com/dkproject/presentation/ui/component/GuestFilterTopBar.kt
@@ -1,46 +1,58 @@
 package com.dkproject.presentation.ui.component
 
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.dkproject.domain.model.GuestFilter
 import com.dkproject.presentation.R
 import com.dkproject.presentation.extension.toFormattedFilterDate
+import com.dkproject.presentation.model.GuestFilterUiModel
 import java.util.Date
 
 @Composable
 fun GuestFilterTopBar(
     modifier: Modifier = Modifier,
-    guestFilter: GuestFilter
+    guestFilter: GuestFilterUiModel,
+    onNearByClick: () -> Unit = {},
+    onDateClick: () -> Unit = {},
+    onPositionClick: () -> Unit = {}
 ) {
     val positionText = when {
         guestFilter.selectedPosition.isEmpty() ->
             stringResource(R.string.userposition)
         guestFilter.selectedPosition.size == 1 ->
-            guestFilter.selectedPosition.first()
+            stringResource(guestFilter.selectedPosition.first().labelRes)
         else -> {
-            stringResource(R.string.filterposition, guestFilter.selectedPosition.first(), guestFilter.selectedPosition.size - 1)
+            stringResource(R.string.filterposition, stringResource(guestFilter.selectedPosition.first().labelRes), guestFilter.selectedPosition.size - 1)
         }
     }
 
     Row(modifier = modifier, horizontalArrangement = Arrangement.spacedBy(12.dp)) {
-        OutlinedButton(onClick = {}) {
+        val animatedColor by animateColorAsState(if(guestFilter.isNearBy) MaterialTheme.colorScheme.secondaryContainer else Color.Unspecified,
+            label = ""
+        )
+        OutlinedButton(onClick = onNearByClick, colors = ButtonDefaults.outlinedButtonColors(containerColor = animatedColor)) {
             Text(text = stringResource(R.string.nearby))
         }
-        OutlinedButton(onClick = {}) {
+        OutlinedButton(onClick = onDateClick) {
             Text(
                 text = if (guestFilter.selectedDate == null) stringResource(R.string.date)
                 else guestFilter.selectedDate?.toFormattedFilterDate() ?: ""
             )
         }
-        OutlinedButton(onClick = {}) {
+        OutlinedButton(onClick = onPositionClick) {
             Text(
                 text = positionText
             )
@@ -53,6 +65,6 @@ fun GuestFilterTopBar(
 private fun GuestFilterTopBarPreview() {
     GuestFilterTopBar(
         modifier = Modifier.fillMaxWidth(),
-        guestFilter = GuestFilter(selectedDate = Date())
+        guestFilter = GuestFilterUiModel(selectedDate = Date())
     )
 }
