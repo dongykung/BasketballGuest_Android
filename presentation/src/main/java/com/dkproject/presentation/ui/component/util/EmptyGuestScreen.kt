f33393a9f25b4b742aecfe1e11b31a094169768b
kim dong kyung <kdkd422@naver.com>
2025-02-13 18:51:39 +0900

    [Chore] Box - contientAlignment Center 추가

--- presentation/src/main/java/com/dkproject/presentation/ui/component/util/EmptyGuestScreen.kt
+++ presentation/src/main/java/com/dkproject/presentation/ui/component/util/EmptyGuestScreen.kt
@@ -3,6 +3,7 @@ package com.dkproject.presentation.ui.component.util
 import androidx.compose.foundation.layout.Box
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import com.dkproject.presentation.R
@@ -11,7 +12,7 @@ import com.dkproject.presentation.R
 fun EmptyGuestScreen(
     modifier: Modifier = Modifier
 ) {
-    Box(modifier = modifier) {
+    Box(modifier = modifier, contentAlignment = Alignment.Center) {
         Text(text = stringResource(R.string.emptyguest))
     }
 }
