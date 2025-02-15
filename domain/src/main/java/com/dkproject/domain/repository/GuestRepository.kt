8899c11cb9e5d057589bef302413e74c23feae89
kim dong kyung <kdkd422@naver.com>
2025-02-15 19:04:00 +0900

    [DI] getPostUserStatus Repository

--- domain/src/main/java/com/dkproject/domain/repository/GuestRepository.kt
+++ domain/src/main/java/com/dkproject/domain/repository/GuestRepository.kt
@@ -3,9 +3,11 @@ package com.dkproject.domain.repository
 import androidx.paging.PagingData
 import com.dkproject.domain.model.GuestFilter
 import com.dkproject.domain.model.GuestPost
+import com.dkproject.domain.model.UserStatus
 import kotlinx.coroutines.flow.Flow
 
 interface GuestRepository {
     suspend fun uploadGuestPost(guestPost: GuestPost): Result<Unit>
     fun getGuestPostList(guestFilter: GuestFilter): Flow<PagingData<GuestPost>>
+    suspend fun getPostUserStatus(postUid: String, userUid: String): UserStatus
 }
