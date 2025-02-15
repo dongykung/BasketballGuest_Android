0ebd1fa2fd79bdbd5baeb4cb652a26bc95438bf8
kim dong kyung <kdkd422@naver.com>
2025-02-15 19:00:37 +0900

    [Feat] 유저 데이터 가져오는 UseCase

---
+++ domain/src/main/java/com/dkproject/domain/usecase/auth/GetUserDataUseCase.kt
@@ -0,0 +1,12 @@
+package com.dkproject.domain.usecase.auth
+
+import com.dkproject.domain.model.User
+import com.dkproject.domain.repository.AuthRepository
+
+class GetUserDataUseCase(
+    private val authRepository: AuthRepository
+) {
+    suspend operator fun invoke(userUid: String): User {
+        return authRepository.getUserData(userUid = userUid)
+    }
+}
