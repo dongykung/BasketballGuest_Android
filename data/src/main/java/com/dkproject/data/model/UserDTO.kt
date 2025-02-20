8c764deaf37ad2ea425732db10df5c7540763c7b
kim dong kyung <kdkd422@naver.com>
2025-02-20 15:30:40 +0900

    [Chore] id nullable

--- data/src/main/java/com/dkproject/data/model/UserDTO.kt
+++ data/src/main/java/com/dkproject/data/model/UserDTO.kt
@@ -4,16 +4,16 @@ import com.dkproject.domain.model.User
 import com.google.firebase.firestore.DocumentId
 
 data class UserDTO(
-    @DocumentId val id: String,
-    val nickName: String,
-    val position: List<String>,
-    val weight: Int?,
-    val height: Int?,
-    val profileImageUrl: String
+    @DocumentId val id: String? = null,
+    val nickName: String = "",
+    val position: List<String> = emptyList(),
+    val weight: Int? = null,
+    val height: Int? = null,
+    val profileImageUrl: String = ""
 ) {
     fun toDomain(): User {
         return User(
-            id = id,
+            id = id ?: "",
             nickName = nickName,
             position = position,
             weight = weight,
