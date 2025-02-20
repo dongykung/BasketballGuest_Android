5695152e8a8af45582b3dc20abed3b67e1a53243
kim dong kyung <kdkd422@naver.com>
2025-02-20 15:26:28 +0900

    [Feat] GeustDetail

--- presentation/src/main/java/com/dkproject/presentation/ui/screen/GuestDetail/GuestDetailScreen.kt
+++ presentation/src/main/java/com/dkproject/presentation/ui/screen/GuestDetail/GuestDetailScreen.kt
@@ -1,69 +1,331 @@
 package com.dkproject.presentation.ui.screen.GuestDetail
 
+import android.util.Log
+import android.widget.Toast
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.CalendarMonth
 import androidx.compose.material.icons.filled.MoreHoriz
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.PeopleAlt
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.SportsBasketball
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.dkproject.domain.model.DataState
+import com.dkproject.domain.model.User
+import com.dkproject.domain.model.UserStatus
+import com.dkproject.presentation.R
+import com.dkproject.presentation.extension.startTimeWithEndTime
+import com.dkproject.presentation.extension.toFormattedFilterDate
+import com.dkproject.presentation.extension.toFormattedHomeGuestListString
+import com.dkproject.presentation.model.Position
+import com.dkproject.presentation.ui.component.DefaultProfileImage
 import com.dkproject.presentation.ui.component.button.DefaultButton
+import com.dkproject.presentation.ui.component.util.ErrorScreen
+import com.dkproject.presentation.ui.component.util.LoadingScreen
 import com.dkproject.presentation.ui.theme.AppTheme
 import com.dkproject.presentation.util.GetUserStatusString
 import com.dkproject.presentation.util.guestPostDummy
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.CameraPosition
+import com.naver.maps.map.compose.CameraPositionState
+import com.naver.maps.map.compose.ExperimentalNaverMapApi
+import com.naver.maps.map.compose.MapProperties
+import com.naver.maps.map.compose.Marker
+import com.naver.maps.map.compose.MarkerState
+import com.naver.maps.map.compose.NaverMap
+import com.naver.maps.map.compose.rememberCameraPositionState
+import kotlinx.coroutines.flow.SharedFlow
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun GuestDetailScreen(
     uiState: PostDetailUiState,
+    uiEvent: SharedFlow<DetailUiEvent>,
     snackbarHostState: SnackbarHostState,
+    loseLoginInfo: () -> Unit = {},
+    navPopBackStack: () -> Unit = {},
+    retryAction: () -> Unit = {},
+    applyButton: (UserStatus) -> Unit,
     modifier: Modifier = Modifier
 ) {
-    val postDetail = uiState.postDetail
-    val scrollState = rememberScrollState()
+    val context = LocalContext.current
+    LaunchedEffect(uiEvent) {
+        uiEvent.collect { event ->
+            when (event) {
+                DetailUiEvent.LoseLoginInfo -> {}
+                is DetailUiEvent.ShowSnackbar -> snackbarHostState.showSnackbar(event.message)
+                is DetailUiEvent.NavPopBackStack -> {
+                    Toast.makeText(context, event.message, Toast.LENGTH_SHORT).show()
+                    navPopBackStack()
+                }
+            }
+        }
+    }
+    when (uiState.dataState) {
+        is DataState.Loading -> LoadingScreen(modifier = Modifier.fillMaxSize())
+        is DataState.Success -> GuestDetailScreen(
+            guestDetail = uiState.dataState.data,
+            statusLoading = uiState.statusLoading,
+            navPopBackStack = navPopBackStack,
+            applyButton = applyButton
+        )
+
+        is DataState.Error -> ErrorScreen(
+            retryAction = retryAction,
+            errorMessage = uiState.dataState.message,
+            modifier = Modifier.fillMaxSize()
+        )
+    }
+}
+
+@OptIn(
+    ExperimentalMaterial3Api::class, ExperimentalNaverMapApi::class,
+    ExperimentalLayoutApi::class
+)
+@Composable
+fun GuestDetailScreen(
+    guestDetail: PostDetailDataState,
+    statusLoading: Boolean,
+    navPopBackStack: () -> Unit,
+    applyButton: (UserStatus) -> Unit = {},
+    modifier: Modifier = Modifier
+) {
+    val postDetail = guestDetail.postDetail
+    val userData = guestDetail.writerInfo
+    val userStatus = guestDetail.myStatus
     Box(modifier = modifier) {
         Column(modifier = Modifier) {
-            TopAppBar(title = {},
+            TopAppBar(
+                title = {},
                 navigationIcon = {
-                    IconButton(onClick = {}) {
+                    IconButton(onClick = navPopBackStack) {
                         Icon(
                             Icons.AutoMirrored.Default.ArrowBack,
                             null
                         )
                     }
                 },
-                actions = { IconButton(onClick = {}) { Icon(Icons.Default.MoreHoriz, null) } }
+                actions = { IconButton(onClick = {}) { Icon(Icons.Default.MoreVert, null) } },
+                colors = TopAppBarDefaults.topAppBarColors(containerColor = MaterialTheme.colorScheme.background)
             )
-            Column(modifier = Modifier.fillMaxSize().verticalScroll(scrollState).padding(horizontal = 16.dp)) {
-                Text(text = postDetail.title,
-                    style = MaterialTheme.typography.titleLarge)
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(horizontal = 16.dp)
+                    .verticalScroll(rememberScrollState()),
+                verticalArrangement = Arrangement.spacedBy(12.dp)
+            ) {
+                WriterInfoSection(
+                    userData = userData,
+                    userStatus = userStatus,
+                    modifier = Modifier.fillMaxWidth()
+                )
+                Text(text = postDetail.title, style = MaterialTheme.typography.titleLarge)
+                Row(
+                    modifier = Modifier.padding(top = 8.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(Icons.Default.CalendarMonth, null, tint = Color.LightGray)
+                    Text(
+                        modifier = Modifier.padding(start = 12.dp),
+                        text = postDetail.startDate.toFormattedFilterDate() + " " + startTimeWithEndTime(
+                            postDetail.startDate,
+                            postDetail.endDate
+                        ),
+                        style = MaterialTheme.typography.bodyMedium
+                    )
+                }
+
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Icon(Icons.Default.PeopleAlt, null, tint = Color.LightGray)
+                    Text(
+                        modifier = Modifier.padding(start = 12.dp),
+                        text = "${postDetail.currentMember}/${postDetail.memberCount}",
+                        style = MaterialTheme.typography.bodyMedium
+                    )
+                }
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Icon(Icons.Default.SportsBasketball, null, tint = Color.LightGray)
+                    FlowRow(
+                        modifier = Modifier.padding(start = 12.dp),
+                        horizontalArrangement = Arrangement.spacedBy(8.dp),
+                        verticalArrangement = Arrangement.spacedBy(6.dp)
+                    ) {
+                        postDetail.positions.forEach { position ->
+                            Surface(
+                                shape = RoundedCornerShape(16.dp),
+                                color = Color(0XFFFFF7ED)
+                            ) {
+                                Text(
+                                    modifier = Modifier.padding(horizontal = 12.dp, vertical = 6.dp),
+                                    text = stringResource(Position.fromFirestoreValue(position).labelRes),
+                                    color = MaterialTheme.colorScheme.secondaryContainer
+                                )
+                            }
+                        }
+                    }
+                }
+                Text(modifier = Modifier.padding(top = 8.dp), text = postDetail.description)
+
+                Spacer(modifier = Modifier.height(8.dp))
+                AddressSection(
+                    placeName = postDetail.placeName,
+                    placeAddress = postDetail.placeAddress,
+                    latLng = LatLng(postDetail.lat, postDetail.lng),
+                    modifier = Modifier
+                )
+                Spacer(modifier = Modifier.height(100.dp))
             }
         }
-        DefaultButton(
+        Column(modifier = Modifier.align(Alignment.BottomCenter).background(MaterialTheme.colorScheme.background)) {
+            val buttonString = stringResource(GetUserStatusString(userStatus))
+            HorizontalDivider()
+            DefaultButton(buttonString, onClick = { applyButton(userStatus) },
+                loading = statusLoading, //&& postDetail.currentMember != postDetail.memberCount,
+                modifier = Modifier.padding(horizontal = 16.dp, vertical = 12.dp).fillMaxWidth())
+        }
+    } // end Box
+}
+
+@Composable
+private fun WriterInfoSection(
+    userData: User,
+    userStatus: UserStatus,
+    modifier: Modifier = Modifier
+) {
+    Row(modifier = modifier, verticalAlignment = Alignment.CenterVertically) {
+        if (userData.profileImageUrl.isEmpty()) {
+            DefaultProfileImage(modifier = Modifier.size(36.dp))
+        } else {
+            AsyncImage(
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data(userData.profileImageUrl)
+                    .crossfade(true)
+                    .build(),
+                contentScale = ContentScale.Crop,
+                placeholder = painterResource(R.drawable.placeholderimage),
+                contentDescription = null,
+                modifier = Modifier.clip(CircleShape).size(36.dp)
+            )
+        }
+        Text(
+            userData.nickName,
+            style = MaterialTheme.typography.bodyMedium,
+            modifier = Modifier.padding(start = 12.dp)
+        )
+        Spacer(modifier = Modifier.weight(1f))
+        if (userStatus != UserStatus.OWNER) {
+            TextButton(onClick = {}) {
+                Text(
+                    stringResource(R.string.chat),
+                    color = MaterialTheme.colorScheme.secondaryContainer,
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalNaverMapApi::class)
+@Composable
+fun AddressSection(
+    placeName: String,
+    placeAddress: String,
+    latLng: LatLng,
+    modifier: Modifier = Modifier
+) {
+    val mapProperties by remember { mutableStateOf(MapProperties(maxZoom = 18.0, minZoom = 12.0)) }
+    val cameraPositionState: CameraPositionState = rememberCameraPositionState {
+        position = CameraPosition(LatLng(latLng.latitude, latLng.longitude), 15.0)
+    }
+    Column(modifier = modifier, verticalArrangement = Arrangement.spacedBy(14.dp)) {
+        Text(
+            text = stringResource(R.string.addressInfo),
+            style = MaterialTheme.typography.titleMedium
+        )
+        Row {
+            Text(
+                text = stringResource(R.string.addressname),
+                modifier = Modifier.weight(3f), textAlign = TextAlign.Start,
+                color = Color.Gray,
+                style = MaterialTheme.typography.bodyMedium
+            )
+            Text(
+                text = placeName, modifier = Modifier.weight(8f), textAlign = TextAlign.Start,
+                style = MaterialTheme.typography.bodyMedium
+            )
+        }
+        Row {
+            Text(
+                text = stringResource(R.string.addressdetail),
+                modifier = Modifier.weight(3f), textAlign = TextAlign.Start,
+                color = Color.Gray,
+                style = MaterialTheme.typography.bodyMedium
+            )
+            Text(
+                text = placeAddress, modifier = Modifier.weight(8f), textAlign = TextAlign.Start,
+                style = MaterialTheme.typography.bodyMedium
+            )
+        }
+        NaverMap(
             modifier = Modifier
                 .fillMaxWidth()
-                .align(Alignment.BottomCenter)
-                .padding(vertical = 16.dp, horizontal = 16.dp),
-            title = stringResource(GetUserStatusString(uiState.myStatus)),
-            onClick = {},
-            loading = uiState.isMyStatusLoading
-        )
+                .height(180.dp)
+                .clip(RoundedCornerShape(16.dp)),
+            properties = mapProperties, cameraPositionState = cameraPositionState
+        ) {
+            Marker(
+                state = MarkerState(position = latLng),
+                captionText = placeName
+            )
+        }
     }
 }
 
@@ -73,12 +335,15 @@ fun GuestDetailScreen(
 private fun GuestDetailScreenPreview() {
     AppTheme {
         GuestDetailScreen(
-            uiState = PostDetailUiState(guestPostDummy),
-            snackbarHostState = SnackbarHostState(),
-            modifier = Modifier
-                .fillMaxSize()
-                .padding()
-        )
+            guestDetail =
+            PostDetailDataState(
+                postDetail = guestPostDummy,
+                myStatus = UserStatus.NONE,
+                writerInfo = User(nickName = "김동경")
 
+            ), statusLoading = false,
+            modifier = Modifier.fillMaxSize(),
+            navPopBackStack = {}
+        )
     }
 }
--- presentation/src/main/java/com/dkproject/presentation/ui/screen/GuestDetail/GuestDetailViewModel.kt
+++ presentation/src/main/java/com/dkproject/presentation/ui/screen/GuestDetail/GuestDetailViewModel.kt
@@ -1,51 +1,164 @@
 package com.dkproject.presentation.ui.screen.GuestDetail
 
+import android.content.Context
+import android.util.Log
+import androidx.compose.foundation.interaction.DragInteraction
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.dkproject.domain.model.DataState
 import com.dkproject.domain.model.User
 import com.dkproject.domain.model.UserStatus
+import com.dkproject.domain.usecase.Guest.ApplyGuestUseCase
 import com.dkproject.domain.usecase.Guest.GetPostUserStatusUseCase
 import com.dkproject.domain.usecase.auth.GetUserDataUseCase
+import com.dkproject.presentation.R
+import com.dkproject.presentation.di.ResourceProvider
 import com.dkproject.presentation.model.GuestPostUiModel
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestoreException
+import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.supervisorScope
+import kotlinx.coroutines.withContext
+import okio.IOException
 import javax.inject.Inject
 
+@HiltViewModel
 class GuestDetailViewModel @Inject constructor(
     private val auth: FirebaseAuth,
-    private val getUserDataUser: GetUserDataUseCase,
-    private val getPostUserStatusUseCase: GetPostUserStatusUseCase
-    ): ViewModel() {
+    private val getUserDataUseCase: GetUserDataUseCase,
+    private val getPostUserStatusUseCase: GetPostUserStatusUseCase,
+    private val applyGuestUseCase: ApplyGuestUseCase,
+    private val resourceProvider: ResourceProvider
+) : ViewModel() {
 
-    private val _postDetailUiState = MutableStateFlow(PostDetailUiState())
-    val postDetailUiState: StateFlow<PostDetailUiState> = _postDetailUiState.asStateFlow()
+    private val _uiState = MutableStateFlow(PostDetailUiState())
+    val uiState: StateFlow<PostDetailUiState> = _uiState.asStateFlow()
 
-    fun setPostDetail(postDetail: GuestPostUiModel) {
-       
+    private val _uiEvent = MutableSharedFlow<DetailUiEvent>()
+    val uiEvent = _uiEvent.asSharedFlow()
+
+    init {
+        Log.d("GuestDetailViewModel", "init")
     }
 
-    fun getPostInfo() {
-        viewModelScope.launch {
-            val df = async {  }
+    fun getPostInfo(postDetail: GuestPostUiModel) {
+        val handler = CoroutineExceptionHandler { _, throwable ->
+            errorHandling(throwable)
         }
+        _uiState.update { it.copy(dataState = DataState.Loading) }
+
+        viewModelScope.launch(handler) {
+            val myUid = getCurrentUserId() ?: return@launch
+            val user = async(context = Dispatchers.IO) {
+                getUserDataUseCase(userUid = postDetail.writerUid)
+            }
+            val status = async(context = Dispatchers.IO) {
+                if(postDetail.writerUid == myUid) UserStatus.OWNER
+                else getPostUserStatusUseCase(postUid = postDetail.id ?: "", userUid = myUid)
+            }
+            val result = DataState.Success(
+                PostDetailDataState(
+                    postDetail = postDetail,
+                    writerInfo = user.await(),
+                    myStatus = status.await()
+                )
+            )
+            _uiState.update { it.copy(dataState = result) }
+        }
+    }
+
+    private fun errorHandling(e: Throwable) {
+        viewModelScope.launch {
+             when (e) {
+                is NoSuchElementException -> {
+                    _uiEvent.emit(DetailUiEvent.NavPopBackStack(resourceProvider.getString(R.string.nosuch)))
+                }
+                is IOException, is FirebaseFirestoreException -> {
+                    _uiState.update { it.copy(dataState = DataState.Error(resourceProvider.getString(R.string.networkerror))) }
+                }
+                else -> {
+                    _uiState.update { it.copy(dataState = DataState.Error(resourceProvider.getString(R.string.defaulterror))) }
+                }
+            }
+        }
+    }
+
+    fun applyButton(userStatus: UserStatus) {
+        when(userStatus) {
+            UserStatus.OWNER -> {}
+            UserStatus.GUEST -> {}
+            UserStatus.APPLY -> {}
+            UserStatus.NONE -> applyGuest()
+        }
+    }
+
+    private fun applyGuest() {
+        viewModelScope.launch {
+            val myUid = getCurrentUserId() ?: return@launch
+            val postDetail = getPostDetailData() ?: return@launch
+
+            setStatusLoading(true)
+            try {
+                withContext(context = Dispatchers.IO) {
+                    applyGuestUseCase(postUid = postDetail.postDetail.id ?: "", userUid = myUid)
+                }
+                _uiEvent.emit(DetailUiEvent.ShowSnackbar(resourceProvider.getString(R.string.completeapply)))
+                _uiState.update { it.copy(dataState = DataState.Success(postDetail.copy(myStatus = UserStatus.APPLY)), statusLoading = false) }
+            } catch (e: Exception) {
+                _uiEvent.emit(DetailUiEvent.ShowSnackbar(resourceProvider.getString(R.string.failapply)))
+                setStatusLoading(false)
+            }
+        }
+    }
+
+    private suspend fun getCurrentUserId(): String? {
+        return auth.currentUser?.uid ?: run {
+            _uiEvent.emit(DetailUiEvent.LoseLoginInfo)
+            null
+        }
+    }
+
+    private suspend fun getPostDetailData(): PostDetailDataState? {
+        return when (val currentState = uiState.value.dataState) {
+            is DataState.Success -> currentState.data
+            else -> {
+                _uiEvent.emit(DetailUiEvent.ShowSnackbar("포스트 정보가 유효하지 않습니다."))
+                null
+            }
+        }
+    }
+
+    private fun setStatusLoading(isLoading: Boolean) {
+        _uiState.update { it.copy(statusLoading = isLoading) }
     }
 }
 
+
 data class PostDetailUiState(
     val dataState: DataState<PostDetailDataState> = DataState.Loading,
-    val writerInfo: User = User(),
-    val myStatus: UserStatus = UserStatus.NONE,
-    val isLoading: Boolean = true,
-    val isMyStatusLoading: Boolean = true,
+    val statusLoading: Boolean = false,
 )
+
 data class PostDetailDataState(
-        val postDetail: GuestPostUiModel,
-        val writerInfo: User,
-        val myStatus: UserStatus
+    val postDetail: GuestPostUiModel,
+    val writerInfo: User,
+    val myStatus: UserStatus
 )
+
+sealed class DetailUiEvent {
+    data class ShowSnackbar(val message: String) : DetailUiEvent()
+    data object LoseLoginInfo : DetailUiEvent()
+    data class NavPopBackStack(val message: String) : DetailUiEvent()
+}
