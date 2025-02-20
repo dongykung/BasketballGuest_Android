b0ccbc1100ae4694fd08ff81839f795e2027799f
kim dong kyung <kdkd422@naver.com>
2025-02-20 15:26:03 +0900

    [Feat] 프로필 이미지 없을 시 디폴트 이미지

---
+++ presentation/src/main/java/com/dkproject/presentation/ui/component/DefaultProfileImage.kt
@@ -0,0 +1,29 @@
+package com.dkproject.presentation.ui.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.dkproject.presentation.ui.theme.AppTheme
+
+@Composable
+fun DefaultProfileImage(modifier: Modifier = Modifier) {
+    Surface(shape = CircleShape, modifier = modifier.clip(CircleShape).background(Color.Gray)) {
+        Icon(Icons.Default.Person, null, tint = Color.Gray)
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+private fun DefaultProfileImagePreview() {
+    AppTheme {
+        DefaultProfileImage()
+    }
+}
