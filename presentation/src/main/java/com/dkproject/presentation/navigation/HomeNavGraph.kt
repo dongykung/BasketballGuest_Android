ebd6c0e4995c756a2a69da7d371f5631105f8b23
kim dong kyung <kdkd422@naver.com>
2025-02-20 15:28:21 +0900

    [Feat] GuestDetail NavGraph 추가

--- presentation/src/main/java/com/dkproject/presentation/navigation/HomeNavGraph.kt
+++ presentation/src/main/java/com/dkproject/presentation/navigation/HomeNavGraph.kt
@@ -1,13 +1,15 @@
 package com.dkproject.presentation.navigation
 
 import android.annotation.SuppressLint
+import android.net.Uri
 import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
@@ -17,9 +19,14 @@ import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import androidx.navigation.toRoute
+import com.dkproject.presentation.model.GuestPostUiModel
 import com.dkproject.presentation.ui.screen.Guest.GuestScreen
 import com.dkproject.presentation.ui.screen.Guest.GuestViewModel
+import com.dkproject.presentation.ui.screen.GuestDetail.GuestDetailScreen
+import com.dkproject.presentation.ui.screen.GuestDetail.GuestDetailViewModel
 import com.dkproject.presentation.ui.screen.myPage.MyPageScreen
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 
 @SuppressLint("ComposableDestinationInComposeScope")
 fun NavGraphBuilder.homeNavGraph(
@@ -33,10 +40,41 @@ fun NavGraphBuilder.homeNavGraph(
         GuestScreen(
             uiState = uiState,
             updateGuestFilter = viewModel::updateGuestFilter,
+            onRefresh = viewModel::refreshGuestList,
             snackbarHostState = snackbarHostState,
-            modifier = modifier
+            modifier = modifier,
+            onFilterReset = viewModel::updateGuestFilter,
+            onNavigateToDetail = { post ->
+                val json = Json.encodeToString(post)  // JSON 문자열 생성
+                val encodedJson = Uri.encode(json)
+                navController.navigate(route = Screen.GuestDetail(encodedJson))
+            }
         )
     }
+
+    composable<Screen.GuestDetail> { backStackEntry ->
+        val postDetail: Screen.GuestDetail = backStackEntry.toRoute()
+        val decodedJson = Uri.decode(postDetail.post)
+        val guestPost: GuestPostUiModel = Json.decodeFromString(decodedJson)
+
+        val viewModel: GuestDetailViewModel = hiltViewModel()
+        LaunchedEffect(guestPost) {
+            viewModel.getPostInfo(guestPost)
+        }
+        val uiState by viewModel.uiState.collectAsState()
+        GuestDetailScreen(
+            uiState = uiState,
+            uiEvent = viewModel.uiEvent,
+            snackbarHostState = snackbarHostState,
+            navPopBackStack = {
+                navController.popBackStack()
+            },
+            retryAction = { viewModel.getPostInfo(guestPost) },
+            applyButton = viewModel::applyButton,
+            modifier = Modifier.fillMaxSize()
+        )
+    }
+
     composable(BottomNavItem.Chat.route) {
         Column(modifier = Modifier.fillMaxSize()) {
             Text(
