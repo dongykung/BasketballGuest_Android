795ff23a78700a286376537dfd8948aa3671d9cd
kim dong kyung <kdkd422@naver.com>
2025-02-20 15:22:37 +0900

    [Feat] NaverMap init

--- app/src/main/java/com/dkproject/basketballguest_android/app/App.kt
+++ app/src/main/java/com/dkproject/basketballguest_android/app/App.kt
@@ -3,6 +3,7 @@ package com.dkproject.basketballguest_android.app
 import android.app.Application
 import com.dkproject.basketballguest_android.BuildConfig
 import com.kakao.sdk.common.KakaoSdk
+import com.naver.maps.map.NaverMapSdk
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
@@ -10,5 +11,6 @@ class App: Application(){
     override fun onCreate() {
         super.onCreate()
         KakaoSdk.init(this, BuildConfig.kakao_native_app_key)
+        NaverMapSdk.getInstance(this).client = NaverMapSdk.NaverCloudPlatformClient(BuildConfig.NAVER_MAP_CLIENT_ID)
     }
 }
