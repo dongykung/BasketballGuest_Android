0b1b97be7b275570962eeb28ab86b89b8a956e13
kim dong kyung <kdkd422@naver.com>
2025-03-01 19:25:57 +0900

    [Feat] graph 분류

--- presentation/src/main/java/com/dkproject/presentation/navigation/HomeNavGraph.kt
+++ presentation/src/main/java/com/dkproject/presentation/navigation/HomeNavGraph.kt
@@ -15,6 +15,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
@@ -24,56 +25,18 @@ import com.dkproject.presentation.ui.screen.Guest.GuestScreen
 import com.dkproject.presentation.ui.screen.Guest.GuestViewModel
 import com.dkproject.presentation.ui.screen.GuestDetail.GuestDetailScreen
 import com.dkproject.presentation.ui.screen.GuestDetail.GuestDetailViewModel
+import com.dkproject.presentation.ui.screen.GuestManage.GuestManageScreen
+import com.dkproject.presentation.ui.screen.GuestManage.GuestManageViewModel
 import com.dkproject.presentation.ui.screen.myPage.MyPageScreen
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 
 @SuppressLint("ComposableDestinationInComposeScope")
 fun NavGraphBuilder.homeNavGraph(
-    navController: NavController, snackbarHostState: SnackbarHostState,
+    navController: NavController,
+    snackbarHostState: SnackbarHostState,
     modifier: Modifier = Modifier
 ) {
-
-    composable(BottomNavItem.Guest.route) {
-        val viewModel: GuestViewModel = hiltViewModel()
-        val uiState by viewModel.uiState.collectAsState()
-        GuestScreen(
-            uiState = uiState,
-            updateGuestFilter = viewModel::updateGuestFilter,
-            onRefresh = viewModel::refreshGuestList,
-            snackbarHostState = snackbarHostState,
-            modifier = modifier,
-            onFilterReset = viewModel::updateGuestFilter,
-            onNavigateToDetail = { post ->
-                val json = Json.encodeToString(post)  // JSON 문자열 생성
-                val encodedJson = Uri.encode(json)
-                navController.navigate(route = Screen.GuestDetail(encodedJson))
-            }
-        )
-    }
-
-    composable<Screen.GuestDetail> { backStackEntry ->
-        val postDetail: Screen.GuestDetail = backStackEntry.toRoute()
-        val decodedJson = Uri.decode(postDetail.post)
-        val guestPost: GuestPostUiModel = Json.decodeFromString(decodedJson)
-
-        val viewModel: GuestDetailViewModel = hiltViewModel()
-        LaunchedEffect(guestPost) {
-            viewModel.getPostInfo(guestPost)
-        }
-        val uiState by viewModel.uiState.collectAsState()
-        GuestDetailScreen(
-            uiState = uiState,
-            uiEvent = viewModel.uiEvent,
-            snackbarHostState = snackbarHostState,
-            navPopBackStack = {
-                navController.popBackStack()
-            },
-            retryAction = { viewModel.getPostInfo(guestPost) },
-            applyButton = viewModel::applyButton,
-            modifier = Modifier.fillMaxSize()
-        )
-    }
 
     composable(BottomNavItem.Chat.route) {
         Column(modifier = Modifier.fillMaxSize()) {
