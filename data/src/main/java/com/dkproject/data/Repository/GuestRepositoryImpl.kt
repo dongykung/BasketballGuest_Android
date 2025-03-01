b6a1ca3490de91cc37aeb72a97113d28084b3b7c
kim dong kyung <kdkd422@naver.com>
2025-03-01 19:25:24 +0900

    [Feat] GuestRepository

--- data/src/main/java/com/dkproject/data/Repository/GuestRepositoryImpl.kt
+++ data/src/main/java/com/dkproject/data/Repository/GuestRepositoryImpl.kt
@@ -6,18 +6,26 @@ import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
 import com.dkproject.data.R
+import com.dkproject.data.data.paging.GuestManagePagingSource
 import com.dkproject.data.data.paging.GuestPagingSource
+import com.dkproject.data.model.GuestPostDTO
 import com.dkproject.data.model.toDTO
+import com.dkproject.data.model.toDomain
+import com.dkproject.domain.Error.ErrorType
 import com.dkproject.domain.model.GuestFilter
+import com.dkproject.domain.model.GuestManage
 import com.dkproject.domain.model.GuestPost
+import com.dkproject.domain.model.UnitResult
 import com.dkproject.domain.model.UserPostStatus
 import com.dkproject.domain.model.UserStatus
 import com.dkproject.domain.model.fromFirestoreValue
 import com.dkproject.domain.repository.GuestRepository
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.FirebaseFirestoreException
+import com.google.firebase.firestore.SetOptions
 import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.merge
 import kotlinx.coroutines.tasks.await
 import okio.IOException
 import javax.inject.Inject
@@ -27,7 +35,7 @@ class GuestRepositoryImpl @Inject constructor(
     @ApplicationContext private val context: Context
 ) : GuestRepository {
     override suspend fun uploadGuestPost(guestPost: GuestPost): Result<Unit> = kotlin.runCatching {
-        firestore.collection("Guest").add(guestPost.toDTO())
+        firestore.collection("Guest").add(guestPost.toDTO()).await()
     }
 
     override fun getGuestPostList(guestFilter: GuestFilter): Flow<PagingData<GuestPost>> {
@@ -61,9 +69,82 @@ class GuestRepositoryImpl @Inject constructor(
     override suspend fun applyGuestPost(postUid: String, userUid: String) {
         try {
             val data = UserPostStatus("APPLY")
-            firestore.collection("Guest").document(postUid).collection("UserStatus").document(userUid).set(data).await()
+            firestore.collection("Guest").document(postUid).collection("UserStatus")
+                .document(userUid).set(data).await()
         } catch (e: Exception) {
             throw e
         }
+    }
+
+    override suspend fun cancelGuestPost(postUid: String, userUid: String) {
+        try {
+            firestore.collection("Guest").document(postUid).collection("UserStatus")
+                .document(userUid).delete().await()
+        } catch (e: Exception) {
+            throw e
+        }
+    }
+
+    override suspend fun getPostData(postUid: String): GuestPost {
+        try {
+            val snapshot = firestore.collection("Guest").document(postUid).get().await()
+            if (!snapshot.exists()) throw NoSuchElementException()
+            return snapshot.toObject(GuestPostDTO::class.java)?.toDomain() ?: throw Exception()
+        } catch (e: Exception) {
+            throw e
+        }
+    }
+
+    override fun getGuestManageList(postUid: String): Flow<PagingData<GuestManage>> {
+        return Pager(
+            config = PagingConfig(pageSize = 10, initialLoadSize = 10, prefetchDistance = 2),
+            pagingSourceFactory = {
+                GuestManagePagingSource(
+                    firestore = firestore,
+                    postUid = postUid
+                )
+            }
+        ).flow
+    }
+
+    override suspend fun acceptGuestUser(postUid: String, userUid: String): UnitResult {
+        return try {
+            val updateData = UserStatus.GUEST.name
+            firestore.collection("Guest").document(postUid).collection("UserStatus")
+                .document(userUid).update("status", updateData).await()
+            UnitResult.Success
+        } catch (e: FirebaseFirestoreException) {
+            if(e.code == FirebaseFirestoreException.Code.NOT_FOUND) {
+                UnitResult.Error(ErrorType.DOCUMENT_NOT_FOUND)
+            }
+            else UnitResult.Error(ErrorType.UNKNOWN_ERROR)
+        } catch (e: Exception) {
+            UnitResult.Error(ErrorType.UNKNOWN_ERROR)
+        }
+    }
+
+    override suspend fun rejectGuestUser(postUid: String, userUid: String): UnitResult {
+        return try {
+            val updateData = UserStatus.DENIED.name
+            firestore.collection("Guest").document(postUid).collection("UserStatus")
+                .document(userUid).update("status", updateData).await()
+            UnitResult.Success
+        } catch (e: FirebaseFirestoreException) {
+            if(e.code == FirebaseFirestoreException.Code.NOT_FOUND) UnitResult.Error(ErrorType.DOCUMENT_NOT_FOUND)
+            else UnitResult.Error(ErrorType.UNKNOWN_ERROR)
+        }
+        catch (e: Exception) {
+            UnitResult.Error(ErrorType.UNKNOWN_ERROR)
+        }
+    }
+
+    override suspend fun updateGuestPost(postUid: String, guestPost: GuestPost): Result<Unit> =
+        runCatching {
+            firestore.collection("Guest").document(postUid)
+                .set(guestPost.toDTO(), SetOptions.merge()).await()
+        }
+
+    override suspend fun deleteGuestPost(postUid: String): Result<Unit> = runCatching {
+        firestore.collection("Guest").document(postUid).delete().await()
     }
 }
--- domain/src/main/java/com/dkproject/domain/repository/GuestRepository.kt
+++ domain/src/main/java/com/dkproject/domain/repository/GuestRepository.kt
@@ -2,7 +2,9 @@ package com.dkproject.domain.repository
 
 import androidx.paging.PagingData
 import com.dkproject.domain.model.GuestFilter
+import com.dkproject.domain.model.GuestManage
 import com.dkproject.domain.model.GuestPost
+import com.dkproject.domain.model.UnitResult
 import com.dkproject.domain.model.UserStatus
 import kotlinx.coroutines.flow.Flow
 
@@ -11,4 +13,11 @@ interface GuestRepository {
     fun getGuestPostList(guestFilter: GuestFilter): Flow<PagingData<GuestPost>>
     suspend fun getPostUserStatus(postUid: String, userUid: String): UserStatus
     suspend fun applyGuestPost(postUid: String, userUid: String)
+    suspend fun cancelGuestPost(postUid: String, userUid: String)
+    suspend fun getPostData(postUid: String): GuestPost
+    fun getGuestManageList(postUid: String): Flow<PagingData<GuestManage>>
+    suspend fun acceptGuestUser(postUid: String, userUid: String): UnitResult
+    suspend fun rejectGuestUser(postUid: String, userUid: String): UnitResult
+    suspend fun updateGuestPost(postUid: String, guestPost: GuestPost): Result<Unit>
+    suspend fun deleteGuestPost(postUid: String): Result<Unit>
 }
