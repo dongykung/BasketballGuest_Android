f914f5e08b17967d9230a37cc20c63fdf0f1c071
kim dong kyung <kdkd422@naver.com>
2025-03-01 19:19:15 +0900

    [Feat] 게스트 취소 UseCase

---
+++ domain/src/main/java/com/dkproject/domain/usecase/Guest/CancelGuestUseCase.kt
@@ -0,0 +1,11 @@
+package com.dkproject.domain.usecase.Guest
+
+import com.dkproject.domain.repository.GuestRepository
+
+class CancelGuestUseCase(
+    private val guestRepository: GuestRepository
+) {
+    suspend operator fun invoke(postUid: String, userUid: String) {
+        guestRepository.cancelGuestPost(postUid, userUid)
+    }
+}
