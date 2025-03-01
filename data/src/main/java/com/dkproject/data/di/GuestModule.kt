0f5b5f62bb6f07b7e8dc649d00f6644954aac45e
kim dong kyung <kdkd422@naver.com>
2025-03-01 19:24:52 +0900

    [DI] GuestModule

--- data/src/main/java/com/dkproject/data/di/GuestModule.kt
+++ data/src/main/java/com/dkproject/data/di/GuestModule.kt
@@ -4,9 +4,16 @@ import com.dkproject.data.Repository.GuestRepositoryImpl
 import com.dkproject.data.Repository.SearchPlaceRepositoryImpl
 import com.dkproject.domain.repository.GuestRepository
 import com.dkproject.domain.repository.SearchPlaceRepository
+import com.dkproject.domain.usecase.Guest.AcceptGuestUseCase
 import com.dkproject.domain.usecase.Guest.ApplyGuestUseCase
+import com.dkproject.domain.usecase.Guest.CancelGuestUseCase
+import com.dkproject.domain.usecase.Guest.DeleteGuestPostUseCase
+import com.dkproject.domain.usecase.Guest.GetGuestManageListUseCase
 import com.dkproject.domain.usecase.Guest.GetGuestPostListUseCase
+import com.dkproject.domain.usecase.Guest.GetPostDataUseCase
 import com.dkproject.domain.usecase.Guest.GetPostUserStatusUseCase
+import com.dkproject.domain.usecase.Guest.RejectGuestUseCase
+import com.dkproject.domain.usecase.Guest.UpdateGuestPostUseCase
 import com.dkproject.domain.usecase.Guest.UploadGuestPostUseCase
 import dagger.Binds
 import dagger.Module
@@ -35,6 +42,41 @@ object GuestUseCaseModule {
     @Provides
     fun applyGuestPostUseCase(guestRepository: GuestRepository): ApplyGuestUseCase {
         return ApplyGuestUseCase(guestRepository)
+    }
+
+    @Provides
+    fun cancelGuestPostUseCase(guestRepository: GuestRepository): CancelGuestUseCase {
+        return CancelGuestUseCase(guestRepository)
+    }
+
+    @Provides
+    fun provideGetPostDataUseCase(guestRepository: GuestRepository): GetPostDataUseCase {
+        return GetPostDataUseCase(guestRepository)
+    }
+
+    @Provides
+    fun provideGetGuestManageListUseCase(guestRepository: GuestRepository): GetGuestManageListUseCase {
+        return GetGuestManageListUseCase(guestRepository)
+    }
+
+    @Provides
+    fun provideAcceptGuestUserUseCase(guestRepository: GuestRepository): AcceptGuestUseCase {
+        return AcceptGuestUseCase(guestRepository)
+    }
+
+    @Provides
+    fun provideRejectGuestUserUseCase(guestRepository: GuestRepository): RejectGuestUseCase {
+        return RejectGuestUseCase(guestRepository)
+    }
+
+    @Provides
+    fun provideUpdateGuestPostUseCase(guestRepository: GuestRepository): UpdateGuestPostUseCase {
+        return UpdateGuestPostUseCase(guestRepository)
+    }
+
+    @Provides
+    fun provideDeleteGuestPostUseCase(guestRepository: GuestRepository): DeleteGuestPostUseCase {
+        return DeleteGuestPostUseCase(guestRepository)
     }
 }
 
