459bb6f0d84577e86f693b8e116470543be2b780
kim dong kyung <kdkd422@naver.com>
2025-03-01 19:21:41 +0900

    [Feat] 내가 작성한 리스트 UseCase

---
+++ domain/src/main/java/com/dkproject/domain/usecase/MyData/GetMyPostListUseCase.kt
@@ -0,0 +1,14 @@
+package com.dkproject.domain.usecase.MyData
+
+import androidx.paging.PagingData
+import com.dkproject.domain.model.GuestPost
+import com.dkproject.domain.repository.MyDataRepository
+import kotlinx.coroutines.flow.Flow
+
+class GetMyPostListUseCase(
+    private val myDataRepository: MyDataRepository
+) {
+    operator fun invoke(myUid: String): Flow<PagingData<GuestPost>> {
+        return myDataRepository.getMyGuestPostList(myUid = myUid)
+    }
+}
