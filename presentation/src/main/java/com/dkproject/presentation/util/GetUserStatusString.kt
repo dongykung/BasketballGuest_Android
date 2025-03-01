91cb6f61c69e4a02f50b76b4c3d4a6d33a22d643
kim dong kyung <kdkd422@naver.com>
2025-03-01 19:22:03 +0900

    [Feat] getGuestManageStatusString

--- presentation/src/main/java/com/dkproject/presentation/util/GetUserStatusString.kt
+++ presentation/src/main/java/com/dkproject/presentation/util/GetUserStatusString.kt
@@ -8,6 +8,16 @@ fun GetUserStatusString(userStatus: UserStatus): Int {
         UserStatus.OWNER -> R.string.manage
         UserStatus.GUEST -> R.string.cancelguest
         UserStatus.APPLY -> R.string.applycancel
+        UserStatus.DENIED -> R.string.rejectguest
         UserStatus.NONE -> R.string.apply
+    }
+}
+
+fun getGuestManageStatusString(userStatus: UserStatus): Int {
+    return when(userStatus) {
+        UserStatus.APPLY -> R.string.applying
+        UserStatus.GUEST -> R.string.guest
+        UserStatus.DENIED -> R.string.denied
+        else -> R.string.etc
     }
 }
