252716ecfc6a7924ae51c274662122e94580f1de
kim dong kyung <kdkd422@naver.com>
2025-03-04 16:22:32 +0900

    [Feat] UserPostStatusDTO

---
+++ data/src/main/java/com/dkproject/data/model/UserPostStatusDTO.kt
@@ -0,0 +1,14 @@
+package com.dkproject.data.model
+
+import com.dkproject.domain.model.UserPostStatus
+import com.google.firebase.firestore.ServerTimestamp
+import java.util.Date
+
+data class UserPostStatusDTO(
+    val status: String = "",
+    @ServerTimestamp val createdAt: Date? = null
+) {
+    fun toDomain(): UserPostStatus {
+        return UserPostStatus(status = status)
+    }
+}
