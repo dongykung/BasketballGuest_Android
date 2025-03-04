7e9942e7d728ac410b4b28eeee169ee2f606cf76
kim dong kyung <kdkd422@naver.com>
2025-03-04 16:16:18 +0900

    [Feat] 내 참여 목록 UseCase

---
+++ domain/src/main/java/com/dkproject/domain/usecase/MyData/GetMyParticipantListUseCase.kt
@@ -0,0 +1,14 @@
+package com.dkproject.domain.usecase.MyData
+
+import androidx.paging.PagingData
+import com.dkproject.domain.model.MyParticipant
+import com.dkproject.domain.repository.MyDataRepository
+import kotlinx.coroutines.flow.Flow
+
+class GetMyParticipantListUseCase(
+    private val myDataRepository: MyDataRepository
+) {
+    operator fun invoke(myUid: String): Flow<PagingData<MyParticipant>> {
+        return myDataRepository.getMyParticipantList(myUid = myUid)
+    }
+}
