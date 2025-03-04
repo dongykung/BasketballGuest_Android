8429b07e92ea7ea512882dc049a0af6386caee1f
kim dong kyung <kdkd422@naver.com>
2025-03-04 16:15:11 +0900

    [Chore] CheckNicknameUseCase 주석 추가

--- domain/src/main/java/com/dkproject/domain/usecase/auth/CheckNicknameUseCase.kt
+++ domain/src/main/java/com/dkproject/domain/usecase/auth/CheckNicknameUseCase.kt
@@ -2,9 +2,13 @@ package com.dkproject.domain.usecase.auth
 
 import com.dkproject.domain.repository.AuthRepository
 
+/**
+ * true -> 닉네임 사용 가능
+ */
 class CheckNicknameUseCase(
     private val authRepository: AuthRepository
 ) {
+    /// true -> 닉네임 사용 가능
     suspend operator fun invoke(nickName: String): Result<Boolean> = kotlin.runCatching {
         authRepository.checkUserNickName(nickName)
     }
