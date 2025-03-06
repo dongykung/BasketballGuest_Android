dfaee3c84f0ae47a67f1da9b113f89c75473092c
kim dong kyung <kdkd422@naver.com>
2025-03-06 20:58:48 +0900

    [Feat] ChatDTO 모델 (파이어베이스 모델)

---
+++ data/src/main/java/com/dkproject/data/model/ChatDTO.kt
@@ -0,0 +1,34 @@
+package com.dkproject.data.model
+
+import com.dkproject.domain.model.Chat.Chat
+import com.google.firebase.firestore.DocumentId
+import com.google.firebase.firestore.ServerTimestamp
+import java.util.Date
+
+data class ChatDTO(
+    @DocumentId val id: String? = null,
+    var message: String = "",
+    var sender: String = "",
+    var readBy: List<String> = emptyList(),
+    @ServerTimestamp var createAt: Date ?= null,
+) {
+    fun toChatEntity(chatRoomId: String): ChatEntity {
+        return ChatEntity(
+            id = id ?: "",
+            chatRoomId = chatRoomId,
+            message = message,
+            sender = sender,
+            readBy = readBy,
+            createAt = createAt ?: Date()
+        )
+    }
+}
+
+fun Chat.toDTO(): ChatDTO {
+    return ChatDTO(
+        id = id,
+        message = message,
+        sender = sender,
+        readBy = readBy,
+    )
+}
