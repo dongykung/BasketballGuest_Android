6997155b0eac92856cc5cd85a8bf64e4d73a1d08
kim dong kyung <kdkd422@naver.com>
2025-03-06 21:03:34 +0900

    [Feat] 채팅 리스트를 가져오는 UseCase

---
+++ domain/src/main/java/com/dkproject/domain/usecase/Chat/GetChatListUseCase.kt
@@ -0,0 +1,14 @@
+package com.dkproject.domain.usecase.Chat
+
+import androidx.paging.PagingData
+import com.dkproject.domain.model.Chat.Chat
+import com.dkproject.domain.repository.ChatRepository
+import kotlinx.coroutines.flow.Flow
+
+class GetChatListUseCase(
+    private val chatRepository: ChatRepository
+) {
+    operator fun invoke(chatRoomId: String): Flow<List<Chat>> {
+        return chatRepository.getChatsByChatRoomId(chatRoomId = chatRoomId)
+    }
+}
