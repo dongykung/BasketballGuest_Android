ee7437c900b911d649d9ae15c0e1470a298b5dbb
kim dong kyung <kdkd422@naver.com>
2025-03-06 21:09:49 +0900

    [Feat] 채팅방 뷰모델 앱 시작시 인스턴스 생성 -> 하단 바 읽지 않은 메시지 수 표시하기 위함

--- presentation/src/main/java/com/dkproject/presentation/activity/MainActivity.kt
+++ presentation/src/main/java/com/dkproject/presentation/activity/MainActivity.kt
@@ -10,38 +10,59 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.compose.rememberNavController
 import com.dkproject.presentation.navigation.AppNavGraph
 import com.dkproject.presentation.ui.component.BottomNavigation
 import com.dkproject.presentation.ui.component.HomeTopAppBar
 import com.dkproject.presentation.ui.component.button.MyFloatingButton
+import com.dkproject.presentation.ui.screen.ChatRoom.ChatRoomUiEvent
+import com.dkproject.presentation.ui.screen.ChatRoom.ChatRoomViewModel
 import com.dkproject.presentation.ui.screen.searchAddress.SearchAddressScreen
 import com.dkproject.presentation.ui.theme.AppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity: ComponentActivity() {
+class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             AppTheme {
+                val chatRoomViewModel: ChatRoomViewModel = hiltViewModel()
+                val chatRoomList by chatRoomViewModel.chatRoomList.collectAsStateWithLifecycle()
                 val navController = rememberNavController()
                 val snackbarHostState = remember { SnackbarHostState() }
                 val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
                 Scaffold(
                     modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
-                    topBar = { HomeTopAppBar(navController = navController, scrollBehavior = scrollBehavior) },
-                    bottomBar = { BottomNavigation(navController = navController) },
+                    topBar = {
+                        HomeTopAppBar(
+                            navController = navController,
+                            scrollBehavior = scrollBehavior
+                        )
+                    },
+                    bottomBar = { BottomNavigation(navController = navController, chatRoomList = chatRoomList) },
                     snackbarHost = { SnackbarHost(snackbarHostState) },
                     floatingActionButton = { MyFloatingButton(navController = navController) }
                 ) { innerPadding ->
-                    AppNavGraph(navController = navController,
-                        modifier = Modifier.fillMaxSize().padding(innerPadding),
-                        snackbarHostState = snackbarHostState
+                    AppNavGraph(
+                        navController = navController,
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding),
+                        snackbarHostState = snackbarHostState,
+                        chatRoomList = chatRoomList,
                     )
                 }
             }
