1461e7000f33889b191987f82d7531753bfed18b
kim dong kyung <kdkd422@naver.com>
2025-03-06 20:59:20 +0900

    [Feat] 채팅 리포지토리

---
+++ domain/src/main/java/com/dkproject/domain/repository/ChatRepository.kt
@@ -0,0 +1,12 @@
+package com.dkproject.domain.repository
+
+import androidx.paging.PagingData
+import com.dkproject.domain.model.Chat.Chat
+import com.dkproject.domain.model.UnitResult
+import kotlinx.coroutines.flow.Flow
+
+interface ChatRepository {
+    fun getChatsByChatRoomId(chatRoomId: String): Flow<List<Chat>>
+    fun listenToChats(myUid: String, chatRoomId: String): Flow<Unit>
+    suspend fun sendMessage(chatRoomId: String, chat: Chat): UnitResult
+}
