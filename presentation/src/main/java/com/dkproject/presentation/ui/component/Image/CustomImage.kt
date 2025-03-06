192dfce37c01cf49efc4c4ca2ef41ce02f9fc77d
kim dong kyung <kdkd422@naver.com>
2025-03-06 21:02:10 +0900

    [Feat] CoilAsyncImage 변경

--- presentation/src/main/java/com/dkproject/presentation/ui/component/Image/CustomImage.kt
+++ presentation/src/main/java/com/dkproject/presentation/ui/component/Image/CustomImage.kt
@@ -1,12 +1,13 @@
 package com.dkproject.presentation.ui.component.Image
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import coil.compose.rememberAsyncImagePainter
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.dkproject.presentation.R
 
 @Composable
@@ -15,15 +16,15 @@ fun CustomImage(
     url:String,
 ) {
     Box(modifier = modifier) {
-        Image(
-            painter = rememberAsyncImagePainter(
-                model = url,
-                placeholder = painterResource(id = R.drawable.placeholderimage),
-                error = painterResource(id = R.drawable.placeholderimage)
-            ), contentDescription = "Profile",
-            modifier = modifier,
-            contentScale = ContentScale.Crop
+        AsyncImage(
+            model = ImageRequest.Builder(LocalContext.current)
+                .data(url)
+                .crossfade(true)
+                .build(),
+            contentScale = ContentScale.Crop,
+            placeholder = painterResource(R.drawable.placeholderimage),
+            contentDescription = null,
+            modifier = Modifier
         )
-
     }
 }
