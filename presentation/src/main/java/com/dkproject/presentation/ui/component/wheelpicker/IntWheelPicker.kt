2d5fd0db5beb4f6a56e09c21b4c8766f2c8ea277
kim dong kyung <kdkd422@naver.com>
2025-03-07 20:46:04 +0900

    [Feat] snapshotFlow -> derivedStateOf 변경 메모리 릭 이슈

--- presentation/src/main/java/com/dkproject/presentation/ui/component/wheelpicker/IntWheelPicker.kt
+++ presentation/src/main/java/com/dkproject/presentation/ui/component/wheelpicker/IntWheelPicker.kt
@@ -7,9 +7,12 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.snapping.rememberSnapFlingBehavior
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
@@ -26,27 +29,37 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import com.dkproject.presentation.R
+import com.dkproject.presentation.ui.component.button.DefaultButton
 import com.dkproject.presentation.ui.theme.AppTheme
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.launch
 
+@OptIn(FlowPreview::class)
 @Composable
 fun IntWheelPicker(
     modifier: Modifier = Modifier,
     pickerMaxHeight: Dp = 200.dp,
     currentValue: Int,
     list: List<Int>,
-    onValueChange: (Int) -> Unit = {}
+    onConfirmClick: (Int) -> Unit = {}
 ) {
     val lazyListState = rememberLazyListState(
         initialFirstVisibleItemIndex = list.size / 2
     )
     val coroutineScope = rememberCoroutineScope()
-
+    val currentItem by remember {
+        derivedStateOf {
+            list[lazyListState.firstVisibleItemIndex]
+        }
+    }
     LaunchedEffect(true) {
         val targetIndex = list.indexOf(currentValue)
         if (targetIndex != -1) {
@@ -54,73 +67,67 @@ fun IntWheelPicker(
         }
     }
 
-    Box(
-        modifier = modifier,
-        contentAlignment = Alignment.Center
-    ) {
-
+    Column(modifier = modifier) {
         Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(40.dp)
-                .background(Color(0XFFF2F2F7))
-        )
-        LazyColumn(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(pickerMaxHeight),
-            state = lazyListState,
-            contentPadding = PaddingValues(16.dp, 80.dp),
-            flingBehavior = rememberSnapFlingBehavior(lazyListState = lazyListState),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
+            contentAlignment = Alignment.Center
         ) {
-            itemsIndexed(list) { index, item ->
-                val centerIndex by remember {
-                    derivedStateOf {
-                        lazyListState.firstVisibleItemIndex
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(40.dp)
+                    .background(Color(0XFFF2F2F7))
+            )
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(pickerMaxHeight),
+                state = lazyListState,
+                contentPadding = PaddingValues(16.dp, 80.dp),
+                flingBehavior = rememberSnapFlingBehavior(lazyListState = lazyListState),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                itemsIndexed(list) { index, item ->
+                    val centerIndex by remember {
+                        derivedStateOf {
+                            lazyListState.firstVisibleItemIndex
+                        }
+                    }
+
+                    val isSelected = index == centerIndex
+                    val animatedScale by animateFloatAsState(
+                        targetValue = if (isSelected) 1.5f else 1.0f, label = ""
+                    )
+                    val animatedColor by animateColorAsState(
+                        targetValue = if (isSelected) Color.Black else Color.Gray, label = ""
+                    )
+                    Box(
+                        contentAlignment = Alignment.Center,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(40.dp)
+                            .clickable {
+                                coroutineScope.launch {
+                                    lazyListState.animateScrollToItem(index)
+                                }
+                            }
+                    ) {
+                        Text(
+                            text = "$item",
+                            color = animatedColor,
+                            modifier = Modifier
+                                .scale(animatedScale)
+                        )
                     }
                 }
-
-                val isSelected = index == centerIndex
-                val animatedScale by animateFloatAsState(
-                    targetValue = if (isSelected) 1.5f else 1.0f, label = ""
-                )
-                val animatedColor by animateColorAsState(
-                    targetValue = if (isSelected) Color.Black else Color.Gray, label = ""
-                )
-                Box(
-                    contentAlignment = Alignment.Center,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(40.dp)
-                        .clickable {
-                            coroutineScope.launch {
-                                lazyListState.animateScrollToItem(index)
-                            }
-                        }
-                ) {
-                    Text(
-                        text = "$item",
-                        color = animatedColor,
-                        modifier = Modifier
-                            .scale(animatedScale)
-                    )
-                }
             }
         }
-    }
-    LaunchedEffect(lazyListState) {
-        snapshotFlow {
-            lazyListState.firstVisibleItemIndex
-        }
-            .filterNotNull()
-            .distinctUntilChanged()
-            .collect { currentIndex ->
-                if (currentIndex in list.indices) {
-                    onValueChange(list[currentIndex])
-                }
-            }
+        Spacer(modifier = Modifier.height(24.dp))
+        DefaultButton(modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp),
+            title = stringResource(R.string.complete),
+            onClick = { onConfirmClick(currentItem) })
     }
 }
 
