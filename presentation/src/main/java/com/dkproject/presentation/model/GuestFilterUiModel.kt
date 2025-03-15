7cc243b5511642586478f767ae6c71a24d13bcac
kim dong kyung <kdkd422@naver.com>
2025-03-15 13:19:56 +0900

    [refactor] .copy를 사용하기 때문에 모든 속셩변수 val 로 수정

--- presentation/src/main/java/com/dkproject/presentation/model/GuestFilterUiModel.kt
+++ presentation/src/main/java/com/dkproject/presentation/model/GuestFilterUiModel.kt
@@ -5,12 +5,12 @@ import com.dkproject.domain.model.Guest.GuestFilter
 import java.util.Date
 
 data class GuestFilterUiModel(
-    var isNearBy: Boolean = false,
-    var myLocation: Coordinate? = null,
-    var radiusInMeters: Double = 10000.0, // 10km
-    var selectedDate: Date? = null,
-    var selectedPosition: List<Position> = emptyList(),
-    var limit: Int = 10
+    val isNearBy: Boolean = false,
+    val myLocation: Coordinate? = null,
+    val radiusInMeters: Double = 10000.0, // 10km
+    val selectedDate: Date? = null,
+    val selectedPosition: List<Position> = emptyList(),
+    val limit: Int = 10
 ) {
     fun toDomain(): GuestFilter {
         return GuestFilter(
