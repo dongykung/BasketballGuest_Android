096ba3c1ff16fe9b430d8081c5dbc817095f4ccb
kim dong kyung <kdkd422@naver.com>
2025-04-24 18:16:08 +0900

    [Feat] Composable 안에서 LaunchedEffect 안에서 repeatOnLifecycle 중복 코드를 피하기 위한 Flow 확장 함수

---
+++ presentation/src/main/java/com/dkproject/presentation/util/collectOnStarted.kt
@@ -0,0 +1,22 @@
+package com.dkproject.presentation.util
+
+import android.util.Log
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.compose.LocalLifecycleOwner
+import androidx.lifecycle.repeatOnLifecycle
+import kotlinx.coroutines.flow.Flow
+
+@Composable
+fun <T> Flow<T>.collectOnStarted(
+    action: suspend (T) -> Unit
+) {
+    val lifecycleOwner = LocalLifecycleOwner.current
+    LaunchedEffect(this, lifecycleOwner) {
+        lifecycleOwner.lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+            Log.d("GuestDetailViewModel", "collectOnStarted")
+            collect { action(it) }
+        }
+    }
+}
